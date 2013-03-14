4b0b68d331de5f72d04696380bad6b9b92667f6a
Arif Jafer <majafer10@gmail.com>
2013-03-14 20:33:50 +0900

    Home Page Controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    if current_user
-      redirect_to home_path
-    else
-      render layout: false
-    end
+    render layout: false
   end
 end
