f69c82f709dd52b6e6611b535103a56590dc9951
Arif Jafer <majafer10@gmail.com>
2013-05-17 19:24:45 +0900

    Fixed Typo

--- db/migrate/20130517101735_add_category_id_to_all_assignments.rb
+++ db/migrate/20130517101735_add_category_id_to_all_assignments.rb
@@ -1,6 +1,6 @@
 class AddCategoryIdToAllAssignments < ActiveRecord::Migration
   def up
-    Assignments.all.each do |asg|
+    Assignment.all.each do |asg|
       asg.category_id = 0
       asg.save
     end
